0747a6324cb1e4dadc86b9f53d1a3333fb9b6e42
Gabor Ovidiu <ovidiu.gabor200@gmail.com>
2020-12-21 19:12:38 +0200

    commit6

--- proiectPLP.v
+++ proiectPLP.v
@@ -26,8 +26,10 @@ Inductive AExp :=
 | adiv : AExp -> AExp -> AExp
 | aproc: AExp -> AExp -> AExp.
 Inductive BExp :=
+| berror
 | btrue : BExp
 | bfalse : BExp
+| bvar: Var -> BExp
 | blessthan : AExp -> AExp -> BExp
 | bgreaterthan : AExp -> AExp -> BExp
 | bequal : AExp -> AExp -> BExp
@@ -55,10 +57,9 @@ Inductive Values :=
 | assign : Values
 | default : Values
 | naturals: eroareNat -> Values
-| booleans: eroareBool -> Values
-| code : Stmt -> Values.
+| booleans: eroareBool -> Values.
 Inductive Programs :=
-| decl_functie : string -> Parametrii -> Stmt -> Programs
+| decl_functie : string -> Parametrii -> Stmt ->Values-> Programs
 | decl_var_globale: Var -> Programs
 | decl_var_locale: Var -> Programs
 | decl_functie_main : Stmt -> Programs
@@ -67,17 +68,14 @@ Inductive Programs :=
 Inductive Memory :=
   | mem_default : Memory
   | offset : nat -> Memory.
-Definition Env := string -> Memory.
+Definition EnvMem := string -> Memory.
 Definition MemLayer := Memory -> Values.
-Definition Stack := list Env.
+Definition Stack := list EnvMem.
 Inductive Config :=
-  | config : nat -> Env -> MemLayer -> Stack -> Config.
-Inductive instructiuni :=
-| push : Var -> instructiuni
-| pop : Var -> instructiuni
-| front : instructiuni.
-
-Definition Coada := list Values.
+  | config : nat -> EnvMem -> MemLayer -> Stack -> Config.
+Inductive coada: Type :=
+| nil : coada
+| elem : eroareNat -> coada -> coada.
 
 Coercion avar :  string >-> AExp.
 Coercion anum : nat >-> AExp.
@@ -112,6 +110,7 @@ Notation " 'intglobal' A " := (decl_var_globale A) (at level 50).
 Notation " S1 ';*' S2 " := (sequance_program S1 S2)(at level 50).
 Notation " 'Template' '<' S '>'" := (decl_templates S )(at level 49).
 Notation " a '=*' p1 p2 '{' s '}' ":= (lambda a p1 p2 s )(at level 49). 
+Notation "  a ->' c " := (elem a c) (at level 50).
 
 Check For ( "i" :n= 1 ; "i" <=' 11 ; "i" :n= "i" +'1 ) {
       "ok" :n= "ok" +' 1
@@ -125,11 +124,264 @@ Check int "a".
 Check "a":n=3.
 Check int* "a"::n=4.
 Check decl_templates "tip" ;*
-      decl_functie "da" [ "ok";"da" ] ("ok":n= "ok"++') ;*
+      decl_functie "da" [ "ok";"da" ] ("ok":n= "ok"++') (naturals (num 1)) ;*
       intglobal "ok" ;*
       decl_functie_main ( (int* "ok"::n="ok"+'1) ;;
                           lambda "lbd" []["ok"] ("ok":n=1) ;;
+                          Do { 
+                              "ok":n="ok"+'1
+                          }while* ( "ok" =>' 0);;
                           apelare_functie "da" [ "a";"b" ]
                         ).
+Check nil.
+Check ( (num 1) ->' ( (num 2) ->' nil)).
 
 
+
+
+
+
+
+
+(*
+Definition Env := Var -> Values.
+Definition plus_eroareNat (n1 n2 : eroareNat) : eroareNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num v1, num v2 => num (v1 + v2)
+    end.
+
+Definition min_eroareNat (n1 n2 : eroareNat) : eroareNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num n1, num n2 => if Nat.ltb n1 n2
+                        then error_nat
+                        else num (n1 - n2)
+    end.
+
+Definition mul_eroareNat (n1 n2 : eroareNat) : eroareNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num v1, num v2 => num (v1 * v2)
+    end.
+
+Definition div_eroareNat (n1 n2 : eroareNat) : eroareNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | _, num 0 => error_nat
+    | num v1, num v2 => num (Nat.div v1 v2)
+    end.
+
+Definition proc_eroareNat (n1 n2 : eroareNat) : eroareNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | _, num 0 => error_nat
+    | num v1, num v2 => num (v1 - v2 * (Nat.div v1 v2))
+    end.
+Fixpoint aeval_fun (a : AExp) (env : Env) : eroareNat :=
+  match a with
+  | avar v => match (env v) with
+                | naturals n => n
+                | _ => error_nat
+                end
+  | anum v => num v
+  | aplus a1 a2 => (plus_eroareNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | amul a1 a2 => (mul_eroareNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | aminus a1 a2 => (min_eroareNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | adiv a1 a2 => (div_eroareNat  (aeval_fun a1 env) (aeval_fun a2 env))
+  | aproc a1 a2 => (proc_eroareNat (aeval_fun a1 env) (aeval_fun a2 env))
+  end.
+Reserved Notation "A =[ S ]=> N" (at level 70).
+Inductive aeval : AExp -> Env -> eroareNat -> Prop :=
+| const : forall n sigma, anum n =[ sigma ]=> num n
+| var : forall v sigma, avar v =[ sigma ]=>  match (sigma v) with
+                                              | naturals x => x
+                                              | _ => error_nat
+                                              end
+| add : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (plus_eroareNat i1 i2) ->
+    a1 +' a2 =[sigma]=> n
+| times : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (mul_eroareNat i1 i2) ->
+    a1 *' a2 =[sigma]=> n
+| substract : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (min_eroareNat i1 i2) ->
+    a1 -' a2 =[sigma]=> n
+| division : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (div_eroareNat  i1 i2) ->
+    a1 /' a2 =[sigma]=> n
+| modulo : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (proc_eroareNat i1 i2) ->
+    a1 %' a2 =[sigma]=> n
+where "a =[ sigma ]=> n" := (aeval a sigma n).
+Definition env : Env := fun x => undecl.
+Example substract_error : 1 -' 5 =[ env ]=> error_nat.
+Proof.
+  eapply substract.
+  - apply const.
+  - apply const.
+  - simpl. reflexivity.
+Qed.
+
+Definition lt_eroareBool (n1 n2 : eroareNat) : eroareBool :=
+  match n1, n2 with
+    | error_nat, _ => error_bool
+    | _, error_nat => error_bool
+    | num v1, num v2 => boolean (Nat.ltb v1 v2)
+    end.
+Definition gt_eroareBool (n1 n2 : eroareNat) : eroareBool :=
+  match n1, n2 with
+    | error_nat, _ => error_bool
+    | _, error_nat => error_bool
+    | num v1, num v2 => boolean (Nat.ltb v2 v1)
+    end.
+Definition eq_eroareBool (n1 n2 : eroareNat) : eroareBool :=
+  match n1, n2 with
+    | error_nat, _ => error_bool
+    | _, error_nat => error_bool
+    | num v1, num v2 => boolean (Nat.eqb v2 v1)
+    end.
+Definition not_eroareBool (n :eroareBool) : eroareBool :=
+  match n with
+    | error_bool => error_bool
+    | boolean v => boolean (negb v)
+    end.
+
+Definition and_eroareBool (n1 n2 : eroareBool) : eroareBool :=
+  match n1, n2 with
+    | error_bool, _ => error_bool
+    | _, error_bool => error_bool
+    | boolean v1, boolean v2 => boolean (andb v1 v2)
+    end.
+
+Definition or_eroareBool (n1 n2 : eroareBool) : eroareBool :=
+  match n1, n2 with
+    | error_bool, _ => error_bool
+    | _, error_bool => error_bool
+    | boolean v1, boolean v2 => boolean (orb v1 v2)
+    end.
+
+Fixpoint beval_fun (a : BExp) (envnat : Env) : eroareBool :=
+  match a with
+  | btrue => boolean true
+  | bfalse => boolean false
+  | berror => error_bool
+  | bvar v => match (env v) with
+                | booleans n => n
+                | _ => error_bool
+                end
+  | blessthan a1 a2 => (lt_eroareBool (aeval_fun a1 envnat) (aeval_fun a2 envnat))
+  | bgreaterthan a1 a2 => (gt_eroareBool (aeval_fun a1 envnat) (aeval_fun a2 envnat))
+  | bequal a1 a2 => (gt_eroareBool (aeval_fun a1 envnat) (aeval_fun a2 envnat))
+  | bnot b1 => (not_eroareBool (beval_fun b1 envnat))
+  | band b1 b2 => (and_eroareBool (beval_fun b1 envnat) (beval_fun b2 envnat))
+  | bor b1 b2 => (or_eroareBool (beval_fun b1 envnat) (beval_fun b2 envnat))
+  
+end.
+
+Reserved Notation "B ={ S }=> B'" (at level 70).
+Inductive beval : BExp -> Env -> eroareBool -> Prop :=
+| b_error: forall sigma, berror  ={ sigma }=> error_bool
+| b_true : forall sigma, btrue ={ sigma }=> boolean true
+| b_false : forall sigma, bfalse ={ sigma }=> boolean false
+| b_var : forall v sigma, bvar v ={ sigma }=>  match (sigma v) with
+                                                | booleans x => x
+                                                | _ => error_bool
+                                                end
+| b_lessthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = (lt_eroareBool i1 i2) ->
+    a1 <=' a2 ={ sigma }=> b
+| b_greaterthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = (gt_eroareBool i1 i2) ->
+    a1 =>' a2 ={ sigma }=> b
+| b_equal : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = (eq_eroareBool i1 i2) ->
+    a1 ==' a2 ={ sigma }=> b
+| b_not : forall a1 i1 sigma b,
+    a1 ={ sigma }=> i1 ->
+    b = (not_eroareBool i1) ->
+    !'a1 ={ sigma }=> b
+| b_and : forall a1 a2 i1 i2 sigma b,
+    a1 ={ sigma }=> i1 ->
+    a2 ={ sigma }=> i2 ->
+    b = (and_eroareBool i1 i2) ->
+    (a1 &' a2) ={ sigma }=> b 
+| b_or : forall a1 a2 i1 i2 sigma b,
+    a1 ={ sigma }=> i1 ->
+    a2 ={ sigma }=> i2 ->
+    b = (or_eroareBool i1 i2) ->
+    (a1 |' a2) ={ sigma }=> b 
+where "B ={ S }=> B'" := (beval B S B').
+Example boolean_operation : bnot (100 <=' "n") ={ env }=> error_bool.
+Proof.
+  eapply b_not.
+  eapply b_lessthan.
+  - eapply const.
+  - eapply var.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Reserved Notation "S -{ Sigma }-> Sigma'" (at level 60).
+
+Inductive eval : Stmt -> Env -> Env -> Prop :=
+| e_nat_decl: forall a i x sigma sigma',
+   a =[ sigma ]=> i ->
+   sigma' = (update sigma x (res_nat i)) ->
+   (x :n= a) -{ sigma }-> sigma'
+| e_nat_assign: forall a i x sigma sigma',
+    a =[ sigma ]=> i ->
+    sigma' = (update sigma x (res_nat i)) ->
+    (x :n= a) -{ sigma }-> sigma'
+| e_bool_decl: forall a i x sigma sigma',
+   a ={ sigma }=> i ->
+   sigma' = (update sigma x (res_bool i)) ->
+   (x :b= a) -{ sigma }-> sigma'
+| e_bool_assign: forall a i x sigma sigma',
+    a ={ sigma }=> i ->
+    sigma' = (update sigma x (res_bool i)) ->
+    (x :b= a) -{ sigma }-> sigma'
+| e_seq : forall s1 s2 sigma sigma1 sigma2,
+    s1 -{ sigma }-> sigma1 ->
+    s2 -{ sigma1 }-> sigma2 ->
+    (s1 ;; s2) -{ sigma }-> sigma2
+| e_if_then : forall b s sigma,
+    ifthen b s -{ sigma }-> sigma
+| e_if_then_elsetrue : forall b s1 s2 sigma sigma',
+    b ={ sigma }=> true ->
+    s1 -{ sigma }-> sigma' ->
+    ifthenelse b s1 s2 -{ sigma }-> sigma' 
+| e_if_then_elsefalse : forall b s1 s2 sigma sigma',
+    b ={ sigma }=> false ->
+    s2 -{ sigma }-> sigma' ->
+    ifthenelse b s1 s2 -{ sigma }-> sigma' 
+| e_whilefalse : forall b s sigma,
+    b ={ sigma }=> false ->
+    while b s -{ sigma }-> sigma
+| e_whiletrue : forall b s sigma sigma',
+    b ={ sigma }=> true ->
+    (s ;; while b s) -{ sigma }-> sigma' ->
+    while b s -{ sigma }-> sigma'
+where "s -{ sigma }-> sigma'" := (eval s sigma sigma').
+*)
